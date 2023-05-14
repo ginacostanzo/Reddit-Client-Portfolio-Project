0b8654559a0110cc08d4ecbfcc30863341c0f316
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-05-14 15:26:03 -0400

    Add components to App

--- reddit-project/src/app/App.js
+++ reddit-project/src/app/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { PopularSubreddits } from '../features/subreddits/PopularSubreddits';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReddit } from '@fortawesome/free-brands-svg-icons';
+import { Posts } from '../features/posts/Posts';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Reddit App</h1>
+      <section id="navigation">
+        <FontAwesomeIcon icon={faReddit} size="2xl" style={{color: "#ff4500",}} />
+        <h1>The Best of Reddit</h1>
+        <button>Top</button>
+        <button>Hot</button>
+        <button>Popular</button>
+      </section>
+      <div class="grid">
+        <PopularSubreddits />
+        <Posts />
+      </div>
     </div>
   );
 }
--- reddit-project/src/app/store.js
+++ reddit-project/src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import subredditsReducer from '../features/subreddits/subredditsSlice';
 
 export default configureStore({
   reducer: {
-    
+    popularSubreddits: subredditsReducer,
   },
 });
