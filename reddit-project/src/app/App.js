6d31a3930bb39e5e6b8c49783aa3ad0d85b02aef
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-05-14 15:57:17 -0400

    Create separate component for navigation buttons

--- reddit-project/src/app/App.js
+++ reddit-project/src/app/App.js
@@ -1,20 +1,13 @@
 import './App.css';
 import { PopularSubreddits } from '../features/subreddits/PopularSubreddits';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReddit } from '@fortawesome/free-brands-svg-icons';
 import { Posts } from '../features/posts/Posts';
+import Navigation from '../components/Navigation';
 
 
 function App() {
   return (
     <div className="App">
-      <section id="navigation">
-        <FontAwesomeIcon icon={faReddit} size="2xl" style={{color: "#ff4500",}} />
-        <h1>The Best of Reddit</h1>
-        <button>Top</button>
-        <button>Hot</button>
-        <button>Popular</button>
-      </section>
+      <Navigation />
       <div class="grid">
         <PopularSubreddits />
         <Posts />
---
+++ reddit-project/src/components/Navigation.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReddit } from '@fortawesome/free-brands-svg-icons';
+import { faFire, faUpLong, faArrowTrendUp } from '@fortawesome/free-solid-svg-icons';
+
+export default function Navigation() {
+    return(
+        <section id="navigation">
+        <FontAwesomeIcon icon={faReddit} size="2xl" style={{color: "#ff4500",}} />
+        <h1>The Best of Reddit</h1>
+        <button className="navBtn">Top <FontAwesomeIcon icon={faUpLong} style={{color: "#ffffff",}} /></button>
+        <button className="navBtn">Hot <FontAwesomeIcon icon={faFire} style={{color: "#ffffff",}} /></button>
+        <button className="navBtn">Popular <FontAwesomeIcon icon={faArrowTrendUp} style={{color: "#ffffff",}} /></button>
+      </section>
+    )
+}
