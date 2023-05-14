b01f06ba878df7d535f06129b076c6a189f7a6f2
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-05-14 17:21:05 -0400

    Create search bar component

---
+++ reddit-project/src/features/search/Search.js
@@ -0,0 +1,8 @@
+export default function Search() {
+    return(
+        <section id="search">
+            <label for="searchBar">search posts: </label>
+            <input id="searchBar" type="text" placeholder="Search the best of reddit"></input>
+        </section>
+    )
+}
