3b91fdce24c5ba062b44b979e95dd860712f8656
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-05-14 15:27:55 -0400

    Add Provider and store to index

--- reddit-project/src/index.js
+++ reddit-project/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './app/App';
-import store from './app/stpre';
+import store from './app/store';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
