1562d6a6919b9a2fa729a6ec7240bbbf88429974
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-05-15 12:46:21 -0400

    Add props to search bar and pass search term back to App and postsSlice

--- reddit-project/src/features/search/Search.js
+++ reddit-project/src/features/search/Search.js
@@ -1,8 +1,23 @@
-export default function Search() {
+import { useDispatch } from "react-redux";
+import { loadPostsBySearch } from "../posts/postsSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+export default function Search({onSearchChange, selectedSubreddit}) {
+    const dispatch = useDispatch();
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const term = document.getElementById('searchBar').value;
+        onSearchChange(term);
+        dispatch(loadPostsBySearch(term, selectedSubreddit));
+        }
+
     return(
         <section id="search">
             <label htmlFor="searchBar">search posts: </label>
             <input id="searchBar" type="text" placeholder="Search the best of reddit"></input>
+            <button className="searchBtn" type="submit" onClick={handleSearch}><FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#ffffff",}} /></button>
         </section>
     )
 }
