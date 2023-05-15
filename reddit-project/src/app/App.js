63d4754d43a19b3b27195dd64a332be5ac5475ef
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-05-14 20:20:32 -0400

    Update App to pass props to Navigation

--- reddit-project/src/app/App.js
+++ reddit-project/src/app/App.js
@@ -2,15 +2,32 @@ import './App.css';
 import { PopularSubreddits } from '../features/subreddits/PopularSubreddits';
 import { Posts } from '../features/posts/Posts';
 import Navigation from '../components/Navigation';
+import { useState, useEffect } from 'react';
+import { loadHomepagePosts } from '../features/posts/postsSlice';
+import { useDispatch } from 'react-redux';
 
 
 function App() {
+  const [selectedSubreddit, setSelectedSubreddit] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleSubredditClick = (subreddit) => {
+    setSelectedSubreddit(subreddit);
+  }
+
+  useEffect(() => {
+    // Load posts when the component mounts or when the selectedSubreddit changes
+    if (!selectedSubreddit) {
+      dispatch(loadHomepagePosts());
+    }
+  }, [dispatch, selectedSubreddit]);
+
   return (
     <div className="App">
-      <Navigation />
-      <div class="grid">
-        <PopularSubreddits />
-        <Posts />
+      <Navigation onBtnClick={handleSubredditClick} selectedSubreddit={selectedSubreddit}/>
+      <div className="grid">
+        <PopularSubreddits onSubredditClick={handleSubredditClick} />
+        <Posts selectedSubreddit={selectedSubreddit} />
       </div>
     </div>
   );
--- reddit-project/src/app/store.js
+++ reddit-project/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import subredditsReducer from '../features/subreddits/subredditsSlice';
+import postsReducer from '../features/posts/postsSlice';
 
 export default configureStore({
   reducer: {
     popularSubreddits: subredditsReducer,
+    posts: postsReducer,
   },
 });
